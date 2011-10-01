7abc81f1ab847453637f119af70995e2653522ec
OiNutter <willmckenzie@oinutter.co.uk>
2011-10-01 23:26:16 +0100

    Added JSONHandler functionality

--- lib/grayskull/formats/json_handler.rb
+++ lib/grayskull/formats/json_handler.rb
@@ -1,8 +1,20 @@
+require 'json'
+
 module Grayskull
   
   module Formats
     
     class JSONHandler
+      
+       def self.load(file)
+         loaded = File.open(file)
+         content = loaded.gets nil
+        begin
+          return JSON.parse(content)
+        rescue Exception => e
+          raise e.class, 'File could not be parsed as valid JSON'          
+        end
+      end
       
     end
     
