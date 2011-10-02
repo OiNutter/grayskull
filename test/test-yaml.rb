4960e1569f6b4ed76d4b3fdcbbf747c2264e71e2
OiNutter <willmckenzie@oinutter.co.uk>
2011-10-02 01:29:59 +0100

    Added test for passing preloaded data

--- test/test-yaml.rb
+++ test/test-yaml.rb
@@ -1,3 +1,4 @@
+require 'yaml'
 require_relative '../lib/grayskull/formats/yaml_handler'
 require_relative '../lib/grayskull/formats'
 require_relative '../lib/grayskull/validator'
@@ -5,7 +6,7 @@ require_relative '../lib/grayskull/validator'
 
 path = File.expand_path(File.join(File.dirname(__FILE__), "yaml/file.yml"))
 schema = File.expand_path(File.join(File.dirname(__FILE__), "yaml/schema.yml"))
+yaml = YAML.load(path)
+validator = Grayskull::Validator.new(yaml,schema)
 
-validator = Grayskull::Validator.new(path,schema)
-
-validator.validate
+puts validator.validate
